faa865b5e790158337f650581ffbf29a3781dd2c
vnaumq <vnaumovich123@gmail.com>
2025-08-28 18:51:34 +0300

    stable version 1.2 "adding MinIO and scripts and pgadmin"

---
+++ dags/config.py
@@ -0,0 +1,10 @@
+import os
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
+MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
+PG_HOST =  os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DB = os.getenv("PG_DB")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -1,30 +1,70 @@
+from config import MINIO_ENDPOINT, MINIO_ROOT_USER, MINIO_ROOT_PASSWORD, PG_HOST, PG_PORT, PG_DB, PG_USER, PG_PASSWORD
 from airflow.decorators import dag, task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime, timedelta
+import boto3
+from sqlalchemy import create_engine
+import pandas as pd
 
-def  main():
-    print('Hello World')
+def main():
+
+    BUCKET_NAME  = "airflow-bucket"
+    KEY = 'file.txt'
+    TABLE_NAME = 'test'
+    SCHEMA_NAME = 'dbo'
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=MINIO_ENDPOINT,
+        aws_access_key_id=MINIO_ROOT_USER,
+        aws_secret_access_key=MINIO_ROOT_PASSWORD,
+        region_name="us-east-1"
+    )
+
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=KEY,
+        Body="Текст",
+        ContentType="text/plain"
+    )
+
+    obj = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
+    content = obj["Body"].read().decode("latin1")  # получаем строку 'Текст'
+    df = pd.DataFrame([content], columns=["text"])  # один ряд и один столбец
+
+    engine = create_engine(
+        f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+    )
+
+    df.to_sql(TABLE_NAME, engine, schema=SCHEMA_NAME, if_exists="replace", index=False)
+    print(f"✅ Загружено {len(df)} строк в таблицу {TABLE_NAME}")
+    print("SUCCESS")
 
     return 0
 
+
 args = {
-    'email': [],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'params': {
-        'priority': 'P0'
-    }
+    "email": [],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "params": {"priority": "P0"},
 }
-@dag(dag_id='test_dag',
-     schedule= None,
-     start_date=datetime(year=2025, month=8, day=26),
-     catchup=False,
-     dagrun_timeout=timedelta(minutes=59),
-     default_args=args,
-     tags=['TEST'])
+
+@dag(
+    dag_id="test_dag",
+    schedule=None,
+    start_date=datetime(year=2025, month=8, day=26),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=59),
+    default_args=args,
+    tags=["TEST"],
+)
 def my_dag():
     @task
     def process():
         main()
+
     process()
+
+
 my_dag()
