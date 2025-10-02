4bb5fb0069a25e138b2a8c0889548b8d2854ae80
vnaumq <vnaumovich123@gmail.com>
2025-10-02 17:48:08 +0300

    update superset

--- init_postgres/02_superset.sql
+++ init_postgres/02_superset.sql
@@ -1,3 +1,15 @@
-CREATE DATABASE superset;
-CREATE USER superset WITH PASSWORD 'superset';
+-- Создание базы данных superset если не существует
+SELECT 'CREATE DATABASE superset'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'superset')\gexec
+
+-- Создание пользователя superset если не существует
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'superset') THEN
+        CREATE USER superset WITH PASSWORD 'superset';
+    END IF;
+END
+$$;
+
+-- Выдача прав
 GRANT ALL PRIVILEGES ON DATABASE superset TO superset;
